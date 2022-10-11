2589fc64a637ccf79440049919b5f25be7eb3f23
KIMHYESU <kimss9209@gmail.com>
2022-10-11 20:01:56 +0900

    ADD: 초기 세팅 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import MovieDetail from "./pages/MovieDetail";
+import Movies from "./pages/Movies";
+
+//1. 페이지는 총 3개 필요(홈페이지,movie,movieDetail)
+//2. 홈페이지에서는 배너를 볼 수 있다
+//3. 3가지 섹션의 영화를 볼 수 있다(popular, top rated, upcoming)
+//4. 영화에 마우스를 올려두면 제목, 장르, 점수, 인기도, 청불 여부 알수있음
+//5. 영화목록을 슬라이드로 넘기면서 다른 영화를 볼 수 있다
+//6. 영화디테일 페이지에서 영화의 정보를 볼 수 있다(포스터,제목,줄거리,점수,인기도..)
+//7. 트레일러를 누르면 트레일러(예고편)를 볼 수 있다
+//8. 영화의 리뷰도 볼 수 있다
+//9. 관련된 영화도 볼 수 있다
+//10. 영화 검색을 할 수 있다
+//11. 영화 정렬 할 수 있다(sort)
+//12. filter도 가능
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+    </>
   );
 }
-
 export default App;
---
+++ src/pages/Movies.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Movies = () => {
+  return <div>Movie 페이지</div>;
+};
+
+export default Movies;
