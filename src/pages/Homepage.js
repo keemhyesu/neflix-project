d78be90c778cec70c06fcca6082135a31798ebcf
KIMHYESU <kimss9209@gmail.com>
2022-10-15 23:13:48 +0900

    ADD: Promise.all(Api 여러개 가져오기), Banner

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
 import { movieAction } from "../redux/action/movieAction";
-import { useDispatch } from "react-redux"; //action을 호출하려면 dispatch가 필요!!!!
+import { useDispatch, useSelector } from "react-redux";
+import Banner from "../components/Banner";
+
 const Homepage = () => {
   const dispatch = useDispatch();
+  const { popularMovies, topRatedMovies, upComingMovies } = useSelector(
+    (state) => state.movie
+  );
+
   useEffect(() => {
     dispatch(movieAction.getMovies());
   }, []);
-  return <div>Homepage</div>;
+  return (
+    <div>
+      {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+    </div>
+  );
 };
 
 export default Homepage;
--- src/redux/action/movieAction.js
+++ src/redux/action/movieAction.js
@@ -4,12 +4,31 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 function getMovies() {
   return async (dispatch) => {
-    const popularMovieApi = await api.get(
+    const popularMovieApi = api.get(
       `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
     );
+
+    const topRatedApi = api.get(
+      `/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    const upComingApi = api.get(`/tv/latest?api_key=${API_KEY}&language=en-US`);
+
     // let url = `/tv/popular?api_key=<<api_key>>&language=en-US&page=1`;
     // let response = await fetch(url);
     // let data = await response.json();
+    let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
+      popularMovieApi,
+      topRatedApi,
+      upComingApi,
+    ]);
+    dispatch({
+      type: "GET_MOVIES_SUCCESS",
+      payload: {
+        popularMovies: popularMovies.data,
+        topRatedMovies: topRatedMovies.data,
+        upComingMovies: upComingMovies.data,
+      },
+    });
   };
 }
 
